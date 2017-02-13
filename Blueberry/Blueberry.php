4392df1c61199680aefee7686eb81f60edc4d9a2
Todd Grimes <toddgrimes@outlook.com>
2017-02-13 12:50:14 -0600

    Added instructions and name to top of file.
    Add the instructions and my name to the top of the file for clarity.

--- Blueberry/Blueberry.php
+++ Blueberry/Blueberry.php
@@ -1,20 +1,34 @@
+<!DOCTYPE html>
+<!-- Written by Todd Grimes <toddgrimes@outlook.com>                         -->
+<!--
+  Write a program that uses a loop to print the integers 1 to 100, one number on
+  each line. When a number is divisible by 3, print "Blue" on the same line, and
+  separate them by a space. When a number is divisible by 5, print "Berry"; and
+  when a number is divisible by 15, print "Blueberry".
+
+  Note that a number divisible by 15 will also be divisible by 3 and 5. In this
+  case, only "Blueberry" should be printed.
+ -->
+
+
+
 <html>
-<head>
-  <title>Blueberry</title>
-</head>
-<body>
+  <head>
+    <title>Blueberry</title>
+  </head>
+  <body>
 <?php
   for ($i = 1; $i <= 100; $i++) {
     if ($i % 15 == 0) {
-      echo $i . " " . "Blueberry<br>";
+      echo "$i Blueberry<br>";
     } else if ($i % 5 == 0) {
-      echo $i . " " . "Berry<br>";
+      echo "$i Berry<br>";
     } else if ($i % 3 == 0) {
-      echo $i . " " . "Blue<br>";
+      echo "$i Blue<br>";
     } else {
-      echo $i . "<br>";
+      echo "$i<br>";
     }
   }
 ?>
-</body>
+  </body>
 </html>
